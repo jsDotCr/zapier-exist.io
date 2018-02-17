8979bda6c103b270b2fd342a82283702fd255d49
jsDotCr <rocco@js.cr>
2018-02-17 19:47:05 +0100

    feat(authentication): use exist API data, add tests

--- authentication.js
+++ authentication.js
@@ -1,33 +1,28 @@
 const getAccessToken = (z, bundle) => {
-  const promise = z.request(`${process.env.BASE_URL}/access-token`, {
+  const promise = z.request(`https://exist.io/oauth2/access_token`, {
     method: 'POST',
     body: {
       code: bundle.inputData.code,
       client_id: process.env.CLIENT_ID,
       client_secret: process.env.CLIENT_SECRET,
-      grant_type: 'authorization_code'
+      grant_type: 'authorization_code',
+      redirect_uri: bundle.inputData.redirect_uri,
     },
     headers: {
       'content-type': 'application/x-www-form-urlencoded'
     }
-  });
+  })
 
-  // Needs to return at minimum, `access_token`, and if your app also does refresh, then `refresh_token` too
   return promise.then((response) => {
     if (response.status !== 200) {
-      throw new Error('Unable to fetch access token: ' + response.content);
+      throw new Error('Unable to fetch access token: ' + response.content)
     }
-
-    const result = JSON.parse(response.content);
-    return {
-      access_token: result.access_token,
-      refresh_token: result.refresh_token
-    };
-  });
-};
+    return JSON.parse(response.content)
+  })
+}
 
 const refreshAccessToken = (z, bundle) => {
-  const promise = z.request(`${process.env.BASE_URL}/refresh-token`, {
+  const promise = z.request(`https://exist.io/oauth2/access_token`, {
     method: 'POST',
     body: {
       refresh_token: bundle.authData.refresh_token,
@@ -38,68 +33,46 @@ const refreshAccessToken = (z, bundle) => {
     headers: {
       'content-type': 'application/x-www-form-urlencoded'
     }
-  });
+  })
 
-  // Needs to return `access_token`. If the refresh token stays constant, can skip it. If it changes, can
-  // return it here to update the user's auth on Zapier.
   return promise.then((response) => {
     if (response.status !== 200) {
-      throw new Error('Unable to fetch access token: ' + response.content);
+      throw new Error('Unable to fetch access token: ' + response.content)
     }
 
-    const result = JSON.parse(response.content);
-    return {
-      access_token: result.access_token
-    };
-  });
-};
+    return JSON.parse(response.content)
+  })
+}
 
-const testAuth = (z /*, bundle*/) => {
-  // Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that
-  // every user will have access to, such as an account or profile endpoint like /me.
+const testAuth = (z) => {
   const promise = z.request({
-    method: 'POST',
-    url: `${process.env.BASE_URL}/auth-test`,
-  });
+    method: 'GET',
+    url: `https://exist.io/api/1/users/\$self/today/`,
+  })
 
-  // This method can return any truthy value to indicate the credentials are valid.
-  // Raise an error to show
   return promise.then((response) => {
     if (response.status === 401) {
-      throw new Error('The access token you supplied is not valid');
+      throw new Error('The access token you supplied is not valid')
     }
-    return response;
-  });
-};
+    return response
+  })
+}
 
 module.exports = {
   type: 'oauth2',
   oauth2Config: {
-    // Step 1 of the OAuth flow; specify where to send the user to authenticate with your API.
-    // Zapier generates the state and redirect_uri, you are responsible for providing the rest.
-    // Note: can also be a function that returns a string
     authorizeUrl: {
-      url: `${process.env.BASE_URL}/authorize`,
+      url: `https://exist.io/oauth2/authorize`,
       params: {
         client_id: '{{process.env.CLIENT_ID}}',
-        state: '{{bundle.inputData.state}}',
         redirect_uri: '{{bundle.inputData.redirect_uri}}',
-        response_type: 'code'
+        response_type: 'code',
+        scope: 'read+write',
       }
     },
-    // Step 2 of the OAuth flow; Exchange a code for an access token.
-    // This could also use the request shorthand.
     getAccessToken: getAccessToken,
-    // (Optional) If the access token expires after a pre-defined amount of time, you can implement
-    // this method to tell Zapier how to refresh it.
     refreshAccessToken: refreshAccessToken,
-    // If you want Zapier to automatically invoke `refreshAccessToken` on a 401 response, set to true
     autoRefresh: true
-    // If there is a specific scope you want to limit your Zapier app to, you can define it here.
-    // Will get passed along to the authorizeUrl
-    // scope: 'read,write'
   },
-  // The test method allows Zapier to verify that the access token is valid. We'll execute this
-  // method after the OAuth flow is complete to ensure everything is setup properly.
   test: testAuth
-};
+}
